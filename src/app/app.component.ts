0ef4037a6beaeff7591c9d36344877dec7455c03
Ishaaq Shaik <s555675@nwmissouri.edu>
2024-01-26 21:23:40 -0500

    Display data using ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = "Register Form";
-  firstName = "";
-  lastName = "";
+  // title = "Register Form";
+  // firstName = "";
+  // lastName = "";
 
-  handleSubmit() {
-    alert(`Registered Successfully ${this.firstName} ${this.lastName}`);
-  }
+  // handleSubmit() {
+  //   alert(`Registered Successfully ${this.firstName} ${this.lastName}`);
+  // }
+  cats = [
+    {
+      "name": "Joe",
+      "image": "./assets/cat.jpg",
+      "age": "5 months",
+      "color": "White",
+    },
+    {
+      "name": "leo",
+      "image": "./assets/cat1.jpg",
+      "age": "2 months",
+      "color": "biscute",
+    },
+    {
+      "name": "sony",
+      "image": "./assets/cat2.jpg",
+      "age": "5 months",
+      "color": "White",
+    }
+  ]
+
+
+
 }
+
+
